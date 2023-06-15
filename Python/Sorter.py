0ba520d831bdd4476d0ccf36f1bde6991aeaeec3
Bhumir2503 <bhumir2503@gmail.com>
2023-06-14 22:53:35 -0400

    starting python

---
+++ Python/Sorter.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Sorter:
+    def __init__(self):
+        self.array = []
+        self.array_size = 0
+        self.array_randomizer()
+    
+
+    def array_randomizer(self):
+        min = 0
+
+
